5d112bf37e4177630f97e332098a914dcb3557c2
Ifeanyichukwu Welechi <welechi@icloud.com>
2022-01-09 11:15:37 +0100

    Update 3_presale_migration.js

--- migrations/3_presale_migration.js
+++ migrations/3_presale_migration.js
@@ -2,5 +2,9 @@ const PreSale = artifacts.require("PreSale");
 const Token = artifacts.require("Token");
 
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(PreSale, Token.address, accounts[0], 1, 10000000, 100000);
+    if (network == "test") {
+        deployer.deploy(PreSale, Token.address, accounts[0], 1, 10000000, 100000);
+    } else {
+        deployer.deploy(PreSale, Token.address, accounts[0], 1, 100000000, 100000);
+    }
 };
